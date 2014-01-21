3c1d912c32962a3fce432078f5c61297cd282e41
cloelius <cloelius1@gmail.com>
2014-01-20 22:28:17 -0500

    Updated Latex Files, added a number of images

--- NeutronDripLineEx1.py
+++ NeutronDripLineEx1.py
@@ -9,12 +9,12 @@ def WBE(Z,N):
     a2=17.23
     a3=.697
     a4=22.6
-    equation=(a1*A-a2*A**(2.0/3.0)-a4*(N-Z)**2/A-a3*Z**2/A**(1/3.0))#/A
+    equation=(a1*A-a2*A**(2.0/3.0)-a4*(N-Z)**2/A-a3*Z**2/A**(1/3.0))
     
     return equation
     
-def GetNeutronDrip(A,Z):
-    N=A-Z
+#def GetNeutronDrip(A,Z):
+    #N=A-Z
 Z=Symbol('Z')
 N=Symbol('N')
 Zs=list(range(1,120))
@@ -25,8 +25,18 @@ for z in Zs:
     except: 
         NeutronDrips.append(mpmath.findroot(lambda N: WBE(z,N),z*8))
     print(z)
-plt.plot(Zs,NeutronDrips)
+NeutronMax=[]
 
+    
+Nn=list(range(100))
+plt.plot(Zs,NeutronDrips)
+plt.title("Neutron Drip Line")
+plt.xlabel("Z Value")
+plt.ylabel("N Value at Z Value")
+WBs=[]
+for i in Nn:
+    WBs.append(WBE(8,i))
+#plt.plot(Nn,WBs)
 plt.show()
 #print(WBE(Z,N))
 #print(solve(WBE(Z,N),Z))
